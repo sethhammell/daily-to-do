f01fae33c830d076a855c34785176e65bc7cd7a6
Seth Hammell <sethjhammell@gmail.com>
2022-04-25 22:23:23 -0400

    created tests for redux todosSlice

---
+++ src/tests/redux/todosSlice.test.js
@@ -0,0 +1,41 @@
+import todosSlice, { setClientId, setTodos } from "../../redux/reducers/todosSlice";
+
+describe('todosSlice', () => {
+  test('Should return the initial state', () => {
+    expect(todosSlice(undefined, {})).toEqual(
+      {
+        todos: [],
+        clientId: ""
+      }
+    )
+  });
+
+  test('setTodos sets todos to value', () => {
+    const todo = [{
+      id: "1",
+      todoCompletionData: [],
+      clientId: "2",
+      taskName: "3",
+      estimatedTime: 4,
+      daysOfWeek: {}
+    }];
+
+    expect(todosSlice(undefined, setTodos(todo))).toEqual(
+      {
+        todos: todo,
+        clientId: ""
+      }
+    )
+  });
+
+  test('setClientId sets clientId to value', () => {
+    const clientId = "123"
+
+    expect(todosSlice(undefined, setClientId(clientId))).toEqual(
+      {
+        todos: [],
+        clientId: clientId
+      }
+    )
+  });
+});
